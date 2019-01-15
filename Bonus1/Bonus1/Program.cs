e1ffb6691d74142a211994e6736d39d4664e5b9a
Laura <Lhuiefiction@gmail.com>
2019-01-14 20:51:24 -0500

    Second Lab and Bonus 1 exercises

---
+++ Bonus1/Bonus1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    
+            Console.WriteLine("Welcome to the Letter Grade Converter!");
+
+            string userinput;
+            userinput = "y";
+
+            double grade;
+
+            while (userinput == "y")
+            {
+                Console.WriteLine("Enter a numerical grade (0-100): ");
+                grade = Convert.ToDouble(Console.ReadLine());
+                if (grade >= 99)
+                {
+                    Console.WriteLine("Letter Grade: A+");
+                }
+                else if (grade < 99 && grade >= 91)
+                {
+                    Console.WriteLine("Letter Grade: A");
+                }
+                else if (grade < 91 && grade >= 88)
+                {
+                    Console.WriteLine("Letter Grade: A-");
+                }
+                else if (grade < 88 && grade >= 86)
+                {
+                    Console.WriteLine("Letter Grade: B+");
+                }
+                else if (grade < 86 && grade >= 82)
+                {
+                    Console.WriteLine("Letter Grade: B");
+                }
+                else if (grade < 82 && grade >=80)
+                {
+                    Console.WriteLine("Letter Grade: B-");
+                }
+                else if (grade < 80 && grade >= 77)
+                {
+                    Console.WriteLine("Letter Grade: C+");
+                }
+                else if (grade < 77 && grade >= 70)
+                {
+                    Console.WriteLine("Letter Grade: C");
+                }
+                else if (grade < 70 && grade >= 67)
+                {
+                    Console.WriteLine("Letter Grade: C-");
+                }
+                else if (grade < 67 && grade >= 64)
+                {
+                    Console.WriteLine("Letter Grade: D+");
+                }
+                else if (grade < 64 && grade >= 63)
+                {
+                    Console.WriteLine("Letter Grade: D");
+                }
+                else if (grade < 63 && grade >= 60)
+                {
+                    Console.WriteLine("Letter Grade: D-");
+                }
+                //worksheet said D=66-60 and F=60-0. Changing F top grade to be 59
+                else
+                {
+                    Console.WriteLine("Letter Grade: F");
+                }
+
+
+
+                Console.WriteLine("Continue? (y/n): ");
+                userinput =  Console.ReadLine().ToLower();
+            }
+            Console.WriteLine("Goodbye");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabTwo/LabTwo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Dertail Generator!");
+            bool run;
+            string userinput;
+
+                                   
+            while (true)
+            {
+                double width, length, area, perimeter, volume, height;
+                Console.WriteLine("User, type in the length of the room in feet: ");
+                length = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("User, type in the width of the room in feet: ");
+                width = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("User, type in the height of the room in feet: ");
+                height = Convert.ToDouble(Console.ReadLine());
+
+                area = width * length;
+                Console.WriteLine("The area of the room is " + area +" feet");
+                perimeter = (2 * width) + (2 * length);
+                Console.WriteLine("The perimeter of the room is " + perimeter +" feet");
+                volume = length * width * height;
+                Console.WriteLine("The volume of the room is " + volume +" feet");
+                                             
+                                
+                Console.WriteLine("Would you like to know the area, perimeters, and volume of another room? (enter Y/N): ");
+                userinput = Console.ReadLine().ToLower();
+
+
+                
+                if (userinput.Equals("y"))
+                {
+                    run = true;
+                }
+                else
+                {
+                    Console.WriteLine("Alright, have a good day");
+                    break;
+                }
+            }
+        
+    
+        }
+    }
+}
